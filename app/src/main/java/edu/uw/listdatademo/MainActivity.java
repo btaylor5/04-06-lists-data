caa04967c9fe5679eb39fc2525e64e6be5b57ab7
Bryant Taylor <btaylor5@uw.edu>
2016-04-12 10:45:35 -0700

    dirty commit. Don't know how far a long this is

--- app/src/main/java/edu/uw/listdatademo/MainActivity.java
+++ app/src/main/java/edu/uw/listdatademo/MainActivity.java
@@ -1,17 +1,125 @@
 package edu.uw.listdatademo;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private ArrayAdapter<String> adapter;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.input_control_layout);
+        setContentView(R.layout.list_layout);
 
+//        // The listener below is used if we don't want to define onclick events in XML
+//        Button btn = (Button) findViewById(R.id.btnSearch);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.v(TAG, "Button Clicked!!");
+//            }
+//        });
+
+//        String[] data = new String[99];
+//        for (int i = 99; i > 0; i--) {
+//            data[99-i] = i + " Bottles of beer on the wall";
+//        }
+
+        MovieDownloadTask task = new MovieDownloadTask();
+        task.execute("Titanic");
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, R.id.txtItem, new ArrayList<String>());
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    public void handleClick(View v) {
+        Log.v(TAG, "Button handled");
+
+        EditText text = (EditText) findViewById(R.id.txtSearch);
+        String searchTerm = text.getText().toString();
+
+        Log.v(TAG, "You search for: " + searchTerm);
+    }
+
+    public class MovieDownloadTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... params) {
+            return MovieDownloader.downloadMovieData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String[] movies) {
+            super.onPostExecute(movies);
+            adapter.clear();
+            for (String movie : movies) {
+                adapter.add(movie);
+            }
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.uw.listdatademo/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://edu.uw.listdatademo/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
